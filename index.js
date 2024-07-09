2351605c40fe082fca37f58bf4909a9d82ce1855
leandryscsec <leandryscsec@gmail.com>
2024-07-09 10:57:50 -0400

    Add changeColor function

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function generateSquares(amount) {
         for (let column = 0; column < amount; column++) {
             const square = document.createElement("div");
             square.className = "grid__container-row-square";
+            square.id = `${Math.floor(Math.random() * 13627)}`;
             row.appendChild(square);
         }
 
@@ -15,4 +16,23 @@ function generateSquares(amount) {
     }
 }
 
-generateSquares(32);
+function changeColor() {
+    const gridContainer = document.querySelector(".grid__container");
+
+    // generate random color when the square is hovered
+    gridContainer.addEventListener("mouseover", (cell) => {
+        const square = cell.target;
+
+        if (square.className !== "grid__container") {
+            square.style.backgroundColor = `rgb(
+                ${Math.floor(Math.random() * 100) * 3},
+                ${Math.floor(Math.random() * 100) * 3},
+                ${Math.floor(Math.random() * 100) * 3})`;
+        }
+    });
+}
+
+// TODO create a function to increase the opacity of the squares
+
+generateSquares(16);
+changeColor();
